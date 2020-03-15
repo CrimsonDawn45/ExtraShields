1c8860acc0ae786e8401ebeca67f8d3229f9c76f
Spaceman <rionlion100@gmail.com>
2020-03-15 11:41:50 -0400

    initialize

---
+++ src/main/java/rionlion100/extrashields/ExtraShields.java
@@ -0,0 +1,10 @@
+package rionlion100.extrashields;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExtraShields implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
