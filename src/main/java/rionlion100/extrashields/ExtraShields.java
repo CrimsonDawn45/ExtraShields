0a9c5c3e424ff4f16bde842c98b703b3586d34a1
Spaceman <rionlion100@gmail.com>
2020-03-19 16:56:53 -0400

    Shield Models

--- src/main/java/rionlion100/extrashields/ExtraShields.java
+++ src/main/java/rionlion100/extrashields/ExtraShields.java
@@ -6,6 +6,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ExtraShields implements ModInitializer {
 
@@ -15,11 +16,25 @@ public class ExtraShields implements ModInitializer {
 		return null;
 	}	
 
+	// public void modIntegrationShields() {
+	// 	FabricLoader loader = FabricLoader.getInstance();
+
+	// 	if (loader.isModLoaded("adabraniummod")) {
+	// 		registerShield("vibranium",10000,ModItems.VIBRANIUM_INGOT);
+	// 		registerShield("adamantium",50000,ModItems.ADAMANTIUM_INGOT);
+	// 	}
+	// }
+
 	@Override
 	public void onInitialize() {
 		registerShield("oak",128,Items.OAK_PLANKS);
 		registerShield("birch",128,Items.BIRCH_PLANKS);
+		registerShield("acacia",128,Items.ACACIA_PLANKS);
+		registerShield("dark_oak",128,Items.DARK_OAK_PLANKS);
+		registerShield("jungle",128,Items.JUNGLE_PLANKS);
+		registerShield("spruce",128,Items.SPRUCE_PLANKS);
 		registerShield("gold",424,Items.GOLD_INGOT);
 		registerShield("diamond",612,Items.DIAMOND);
+		//modIntegrationShields();
 	}
 }
